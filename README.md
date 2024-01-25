de9c4460cdb7ffa151ec13177162e795bef9a4e3
uphoon <uphoon@gmail.com>
2024-01-25 14:19:09 +0900

    equalizer

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # equalizer
+
 Interactive Flutter Equalizer Animation
+
+
+펄싱 바를 시각적으로 모방하는 동적인 플러터 애플리케이션을 호스트합니다. CustomPainter와 AnimationController의 힘을 이용하여, 사용자의 상호 작용에 반응하여 간단한 탭으로 애니메이션을 시작하고 중지합니다.
+
+
+## Features
+
++ Interactive animation toggle through user input
++ Customizable bar count and dimensions for varied visual appeal
++ Smooth and eye-catching animations with rounded stroke caps
++ Optimized animation performance for seamless user experience
+
+
+## screenshot
+
+![equalizer_1](https://github.com/uphoon/equalizer/assets/3182587/7dfeeb7b-4ac2-472a-991f-536d005691f8)
+
+
+## show into dartpad
+
+<iframe src="https://dartpad.dev/embed-flutter.html?id=d45563969f8b818d541ce39051decba2" style="width:100%; height:700px"></iframe>
+
+
+## Contributions
+
+Contributions are welcome! If you have ideas on how to improve the animation or extend the functionality, feel free to fork the repository and submit your pull requests.
+
+개선 아이디어가 있거나 기능을 확장하고 싶은 분들의 기여를 환영합니다! 저장소를 포크하고 풀 리퀘스트를 제출하여 자유롭게 기여해주세요.
+
+
+*writed by ChatGPT*
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+/*
+ * Author: Yi Sanghoon
+ * Email: uphoon@gmail.com
+ * Created Date: 2024-01-25
+ * Last Modified: 2024-01-25
+ * Description:
+ *  This file is used for creating the Equalizer animation
+ * using CustomPainter and AnimationController 
+ */
+
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Equalizer()))));
+}
+
+class Equalizer extends StatefulWidget {
+  const Equalizer({super.key});
+
+  @override
+  _EqualizerState createState() => _EqualizerState();
+}
+
+class _EqualizerState extends State<Equalizer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _toggleAnimation,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (_, child) {
+          return CustomPaint(
+            size: const Size(100, 120),
+            painter: EqualizerPainter(_controller.value),
+          );
+        },
+      ),
+    );
+  }
+
+  void _toggleAnimation() {
+    setState(() {
+      isPlaying = !isPlaying;
+      if (isPlaying) {
+        _controller.repeat(reverse: true);
+      } else {
+        _controller.stop();
+      }
+    });
+  }
+}
+
+class EqualizerPainter extends CustomPainter {
+  EqualizerPainter(this.value) : super();
+
+  final double value;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 8
+      ..strokeCap = StrokeCap.round;
+
+    // Define the number of bars you want
+    const barCount = 7;
+
+    // Define the width of each bar
+    final barWidth = size.width / (2 * barCount + 1);
+
+    List<int> startYBars = [0, 20, -60, 40, -10, 30, 5];
+
+    for (int i = 0; i < barCount; i++) {
+      // var heightGap = startYBars[math.Random().nextInt(startYBars.length)];
+      var heightGap = startYBars[i];
+
+      // Randomize the bar height based on the animation value
+      final barHeight = i.isEven
+          ? size.height / 2 * (math.Random().nextDouble() + 0.1) * value
+          : size.height / 2 * (math.Random().nextDouble() + 0.2) * value;
+
+      final barX = barWidth + i * barWidth * 2;
+
+      final startY = size.height / 2 + (barHeight + heightGap) / 2;
+      final endY = size.height / 2 - (barHeight + heightGap) / 2;
+
+      canvas.drawLine(Offset(barX, startY), Offset(barX, endY), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
